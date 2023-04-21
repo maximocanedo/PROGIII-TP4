76c97b0a84bae94ee79cd3710a48f734fbf13bfa
Máximo Canedo <30154899delacroix@gmail.com>
2023-04-21 11:35:43 -0300

    Resolución primer ejercicio

---
+++ Ejercicio1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio1.aspx.cs" Inherits="TrabajoPractico4.Ejercicio1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h3>Origen: </h3>
+        <br />
+        <label for="ProvinciaInicio">
+            <span>Provincia: </span>
+            <br />
+            <asp:DropDownList ID="ProvinciaInicio" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ProvinciaInicio_SelectedIndexChanged"></asp:DropDownList>
+        </label>
+        <br />
+        <label for="LocalidadInicio">
+            <span>Localidad: </span>
+            <br />
+            <asp:DropDownList ID="LocalidadInicio" runat="server" OnSelectedIndexChanged="LocalidadInicio_SelectedIndexChanged"></asp:DropDownList>
+        </label>
+        <br />
+        <h3>Destino: </h3>
+        <br />
+        <label for="ProvinciaDestino">
+            <span>Provincia: </span>
+            <br />
+            <asp:DropDownList ID="ProvinciaDestino" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ProvinciaDestino_SelectedIndexChanged"></asp:DropDownList>
+        </label>
+        <br />
+        <label for="LocalidadDestino">
+            <span>Localidad: </span>
+            <br />
+            <asp:DropDownList ID="LocalidadDestino" runat="server"></asp:DropDownList>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </label>
+    </form>
+</body>
+</html>
---
+++ Ejercicio1.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TrabajoPractico4 {
+    public partial class Ejercicio1 : System.Web.UI.Page {
+        private const string SQL_CONNECTION_STRING = @"Data Source=DESKTOP-G2NKNCB\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True";
+        private void SetProvincias(DropDownList provincias, int aEvitar = -1) {
+            SqlConnection connection = new SqlConnection(SQL_CONNECTION_STRING);
+            connection.Open();
+            string consulta = aEvitar == -1 ?
+                "SELECT * FROM [Provincias]" :
+                "SELECT * FROM [Provincias] WHERE [IdProvincia] != '" + aEvitar + "'";
+            SqlDataAdapter adapter = new SqlDataAdapter(consulta, connection);
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset, "provincias");
+            provincias.DataSource = dataset.Tables["provincias"];
+            provincias.DataTextField = "NombreProvincia";
+            provincias.DataValueField = "IdProvincia";
+            provincias.DataBind();
+            provincias.Items.Insert(0, new ListItem("Seleccione provincia", "Seleccione provincia"));
+            connection.Close();
+        }
+        protected void Page_Load(object sender, EventArgs e) {
+            if(!IsPostBack) {
+                SetProvincias(ProvinciaInicio);
+            }
+        }
+        private void CambioDeProvincia(DropDownList provincia, DropDownList localidad) {
+            int provinciaSeleccionada = int.Parse(provincia.SelectedValue);
+            if (provinciaSeleccionada > 0) {
+                SqlConnection connection = new SqlConnection(SQL_CONNECTION_STRING);
+                connection.Open();
+                string consulta = "SELECT * FROM [Localidades] WHERE [IdProvincia] = '" + (provinciaSeleccionada) + "'";
+                // Label1.Text = consulta;
+                SqlDataAdapter adapter = new SqlDataAdapter(consulta, connection);
+                DataSet dataset = new DataSet();
+                adapter.Fill(dataset, "Localidades");
+                localidad.DataSource = dataset.Tables["Localidades"];
+                localidad.DataTextField = "NombreLocalidad";
+                localidad.DataValueField = "IdLocalidad";
+                localidad.DataBind();
+                localidad.Items.Insert(0, new ListItem("Seleccione localidad", "Seleccione localidad"));
+                connection.Close();
+            }
+
+        }
+        
+        protected void ProvinciaInicio_SelectedIndexChanged(object sender, EventArgs e) {
+            CambioDeProvincia(ProvinciaInicio, LocalidadInicio);
+            if(ProvinciaInicio.SelectedIndex > 0) {
+                SetProvincias(ProvinciaDestino, (ProvinciaInicio.SelectedIndex));
+            }
+        }
+
+        protected void LocalidadInicio_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        protected void ProvinciaDestino_SelectedIndexChanged(object sender, EventArgs e) {
+            CambioDeProvincia(ProvinciaDestino, LocalidadDestino);
+        }
+    }
+}
