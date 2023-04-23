747e2c4bb679028e83f8cb63fb99ead8e08dc7b3
Máximo Canedo <30154899delacroix@gmail.com>
2023-04-23 11:58:41 -0300

    Resolución ejercicio 2

---
+++ Ejercicio2.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio2.aspx.cs" Inherits="TrabajoPractico4.Ejercicio2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <span>Id Producto: </span>
+            <asp:DropDownList ID="IDProductoDDL" runat="server">
+                <asp:ListItem Value="1">Igual a</asp:ListItem>
+                <asp:ListItem Value="2">Mayor a</asp:ListItem>
+                <asp:ListItem Value="3">Menor a</asp:ListItem>
+            </asp:DropDownList>
+            <asp:TextBox ID="IDProductoT" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <span>Id Categoría: </span>
+            <asp:DropDownList ID="IDCategoriaDDL" runat="server">
+                <asp:ListItem Value="1">Igual a</asp:ListItem>
+                <asp:ListItem Value="2">Mayor a</asp:ListItem>
+                <asp:ListItem Value="3">Menor a</asp:ListItem>
+            </asp:DropDownList>
+            <asp:TextBox ID="IDCategoriaT" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="BtnFiltrar" runat="server" Text="Filtrar" OnClick="BtnFiltrar_Click" />
+            <asp:Button ID="BtnQuitarFiltros" runat="server" Text="Quitar filtros" OnClick="BtnQuitarFiltros_Click" />
+        </div>
+        <div>
+            <asp:GridView ID="gvProductos" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Ejercicio2.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TrabajoPractico4 {
+    public partial class Ejercicio2 : System.Web.UI.Page {
+        private const string STRING_CONNECTION = @"Data Source=DESKTOP-G2NKNCB\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+        protected string getProductFilterCommand() {
+            string val = IDProductoT.Text;
+            int filtro = int.Parse(IDProductoDDL.SelectedValue);
+            switch(filtro) {
+                case 1: // Igual a
+                    return "= " + val;
+                case 2: // Mayor a
+                    return "> " + val;
+                case 3: // Menor a
+                    return "< " + val;
+            }
+            return "";
+        }
+        protected string getCategoryFilterCommand() {
+            string val = IDCategoriaT.Text;
+            int filtro = int.Parse(IDCategoriaDDL.SelectedValue);
+            switch (filtro) {
+                case 1: // Igual a
+                    return "= " + val;
+                case 2: // Mayor a
+                    return "> " + val;
+                case 3: // Menor a
+                    return "< " + val;
+            }
+            return "";
+        }
+        protected void cargarDatos(bool filtrar = false) {
+            SqlConnection connection = new SqlConnection(STRING_CONNECTION);
+            connection.Open();
+            string consulta = "SELECT [IdProducto], [NombreProducto], [IdCategoría], [CantidadPorUnidad], [PrecioUnidad] FROM [Productos] ";
+            bool seFiltraProducto = IDProductoT.Text != "";
+            bool seFiltraCategoria = IDCategoriaT.Text != "";
+            bool flt = filtrar && (seFiltraProducto || seFiltraCategoria);
+            if (filtrar) {
+                if (seFiltraProducto) {
+                    consulta += " WHERE [IdProducto] " + getProductFilterCommand();
+                }
+                if(seFiltraCategoria) {                    
+                    consulta += (seFiltraProducto? " AND " : " WHERE ") 
+                        + "[IdCategoría] " + getCategoryFilterCommand();
+                }
+            }
+            SqlDataAdapter adapter = new SqlDataAdapter(consulta, connection);
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset, "Productos");
+            gvProductos.DataSource = dataset.Tables[0];
+            gvProductos.DataBind();
+            connection.Close();
+        }
+        protected void Page_Load(object sender, EventArgs e) {
+            if(!IsPostBack) {
+                cargarDatos();
+            }
+        }
+
+        protected void BtnQuitarFiltros_Click(object sender, EventArgs e) {
+            cargarDatos();
+        }
+
+        protected void BtnFiltrar_Click(object sender, EventArgs e) {
+            cargarDatos(true);
+        }
+    }
+}
